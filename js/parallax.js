da9f18f255a2fb6414cfbde0b737458ab9a9623b
ernestognw <ernestognw@gmail.com>
2018-08-14 19:26:45 -0500

    Change daniel colors

--- js/parallax.js
+++ js/parallax.js
@@ -1,32 +1,31 @@
-function castParallax() {
+// function castParallax() {
 
-	var opThresh = 350;
-  var opFactor = 750;
+// 	var opThresh = 350;
+//   var opFactor = 750;
 
-  update();
+//   update();
 
   window.addEventListener("resize", function() {
-    update();
+    updateIllustrationsWidth();
   });
 
-	window.addEventListener("scroll", function() {
-    update()
-  });
+// 	window.addEventListener("scroll", function() {
+//     update()
+//   });
 
-  window.addEventListener("scroll", function(){
-    aboutImage();
-  })
-}
+//   window.addEventListener("scroll", function(){
+//     aboutImage();
+//   })
+// }
 
 
-function update() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  var ratio = w / h;
+function updateIllustrationsWidth() {
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+  let ratio = w / h;
 
-  var top = this.pageYOffset;
-  var layers = document.getElementsByClassName("parallax");
-  var layer, speed, yPos, backsize;
+  let layers = document.getElementsByClassName("parallax");  
+  let backsize = 0;
 
   if (ratio <= 1024 / 768){
     backsize = 100*((1024/768)/ratio); 
@@ -37,23 +36,50 @@ function update() {
     backsize = '100%'
   }
 
-  for (var i = 0; i < layers.length; i++) {
-    layer = layers[i];
-    speed = layer.getAttribute('data-speed');
-    var yPos = -(top * speed / 100);
-    layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px);' + 'background-size:' + backsize + '; !important');
+  for (let i = 0; i < layers.length; i++) {
+    layer = layers[i]
+    layer.setAttribute('style', 'background-size:' + backsize + '; !important');
   }
 }
 
-function aboutImage() {
-  var aboutImage = document.getElementById('about-image-1')
+// function aboutImage() {
+//   var aboutImage = document.getElementById('about-image-1')
 
-  var yScroll = window.scrollY;
-  yScroll -= 800;
+//   var yScroll = window.scrollY;
+//   yScroll -= 800;
 
-  if(yScroll <= 0){
-    aboutImage.setAttribute('style', 'left:' + yScroll + 'px')
-  }
+//   if(yScroll <= 0){
+//     aboutImage.setAttribute('style', 'left:' + yScroll + 'px')
+//   }
+// }
+
+// document.body.onload = castParallax();			
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build tween
+var aboutImageTween = TweenMax.to("#about-image-1", 0.5, {x: 950});
+// build scene and supply getMousePos function as duration
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 300})
+        .setTween(aboutImageTween)
+        .addIndicators() 
+        .addTo(controller);
+
+
+var layer = [];
+var sceneLayer = [];
+var controllerLayer = [];
+var keyarts = document.getElementsByClassName("parallax");
+
+for (i = 0; i < 9; i++){
+  controllerLayer[i] = new ScrollMagic.Controller(); 
+  let speed = keyarts[i].getAttribute('speed')
+  let yPos = -(window.innerHeight * speed / 100);     
+  layer[i] = TweenMax.to(`#keyart-${i}`, 0.5, {y: -150 * i});
+  console.log(layer[i])
+  sceneLayer[i] = new ScrollMagic.Scene({triggerElement: "#initialTrigger", duration: window.innerHeight*2})
+        .setTween(layer[i])
+        .addIndicators() 
+        .addTo(controllerLayer[i]);
 }
-
-document.body.onload = castParallax();
